2ce51ef3b0d313609626081a4c8bdcc188dadec8
Dylan <122345584+Dyerag@users.noreply.github.com>
2025-06-16 14:48:04 +0200

    begundt ordentligt med at lave spillet, efter endeligt at havde sat up git source control

--- Flappy Bird/Assets/BirdScript.cs
+++ Flappy Bird/Assets/BirdScript.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    public Rigidbody2D Rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+            Rigidbody.velocity = Vector2.up * 10;
     }
 }
