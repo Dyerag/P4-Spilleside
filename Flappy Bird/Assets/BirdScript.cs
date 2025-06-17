de40beb024ddcdea7ec5b2ab82b1acd48ecc3b40
Dylan <122345584+Dyerag@users.noreply.github.com>
2025-06-17 10:30:05 +0200

    Fuglens flagren når mellemrum er trykket, er tilføjet. Rør objektet er tilføjet, men mangler funktion. Rettet .gitignore til også at inkludere .vs/, da den i stedet havde .vscode/, hvilket ikke bruges i Visual Studio.

--- Flappy Bird/Assets/BirdScript.cs
+++ Flappy Bird/Assets/BirdScript.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    // Bird gameObject's komponent for real-time physics
     public Rigidbody2D Rigidbody;
-
+    // En variabel for hvor højt fuglen skal flyve ved hvert tryk. Værdien sættes i unity
+    public float flapStrength;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +17,8 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Læser inputs for om space er trykket
         if (Input.GetKeyDown(KeyCode.Space) == true)
-            Rigidbody.velocity = Vector2.up * 10;
+            Rigidbody.velocity = Vector2.up * flapStrength;
     }
 }
