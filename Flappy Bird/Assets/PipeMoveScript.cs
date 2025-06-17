617aa1ce5966b57de419b8272d82f1915732e12c
Dylan <122345584+Dyerag@users.noreply.github.com>
2025-06-17 14:57:00 +0200

    PipeSpawn virker. Pipes vil nu starte udenfor skærmen, på højreside, og bevæge sig mod venstre. Desværre virker sletningen af pipes, efter de har passeret skærmen, ikke.

--- Flappy Bird/Assets/PipeMoveScript.cs
+++ Flappy Bird/Assets/PipeMoveScript.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 
 public class PipeMoveScript : MonoBehaviour
 {
+    // Hvor hurtigt en pipe kan bevæge sig 
     public float moveSpeed = 5;
+    // Afgrænsningen til at slette objektet
+    public float deadZone = -40;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log($"Start position: {transform.position.x}");
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+
+        if (transform.position.x < deadZone)
+        {
+            Debug.Log("Pipe Deleted");
+            Destroy(gameObject);
+        }
     }
 }
--- Flappy Bird/Assets/PipeSpawnScript.cs
+++ Flappy Bird/Assets/PipeSpawnScript.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class PipeSpawnScript : MonoBehaviour
 {
+    // Pipe objektet der bliver affødt. hentes fra Unity
     public GameObject pipe;
+    // Hvor lang tid der skal gå, før en ny genereres
     public float spawnRate = 2;
+    // Hvor lang tid var gået fra sidste dannelse
     public float timer = 0;
+    // Hvor meget de fremkaldte rør må forskydes
     public float heightOffset = 10;
 
     // Start is called before the first frame update
     void Start()
     {
+        // metoden køres én gang ved start, for at skabe det første rør når spillet begynder, i stedet for at vente
         Spawn();
     }
 
@@ -20,19 +25,26 @@ public class PipeSpawnScript : MonoBehaviour
     {
         if (timer < spawnRate)
         {
+            // timer får tiden fra unity, specifikt får summen af sekunderne mellem hver frame
             timer += Time.deltaTime;
         }
         else
         {
             Spawn();
+            // Nullstilles for igen at tælle 
             timer = 0;
         }
     }
 
+    /// <summary>
+    /// Skaber rør objektet.
+    /// </summary>
     void Spawn()
     {
+        // Bruger forskydelsen og Pipe Spawners position for at finde den laveste og højeste y position et par rør kan ha'.
         float lowestPoint = transform.position.y - heightOffset;
         float highestPoint = transform.position.y + heightOffset;
+        // Skaber et nyt gameObject ved brug af den angivne pipe prefab, x positionen og rotationen af spawneren, og en tilfældig y værdi ved brug af lowestPoint og highestPoint.
         Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
     }
 }
