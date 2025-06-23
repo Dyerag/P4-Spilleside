3c888c6910db950be570bc26c86b420c7a71a585
Dylan <122345584+Dyerag@users.noreply.github.com>
2025-06-23 14:42:45 +0200

    Bird bliver nu frossen når den bevæger sig for langt på y værdien. Det er  for at stoppe den med at bevæge sig når spillet er tabt og programmet stadigvæk kører.
    LogicScript har nu en boolean der stopper point i at fås når Bird er død. Så kan flere point ikke fås ved at falde igennem de næste Pipes.
    
    Flere kommentarer tilføjet

--- Flappy Bird/Assets/BirdScript.cs
+++ Flappy Bird/Assets/BirdScript.cs
@@ -15,6 +15,7 @@ public class BirdScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Finder LogicScript gennem Logic Manager via tag
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
 
     }
@@ -22,15 +23,23 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Læser inputs for om space er trykket
+        // Læser inputs for om space er trykket, og Bird stadigt er i live
         if (Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive)
             Rigidbody.velocity = Vector2.up * flapStrength;
+
+        // Stopper Bird fra at blive ved med at falde
+        if (transform.position.y < -40 || transform.position.y > 40)
+            // Stopper Bird fra at bevæge sig når den er udenfor Play Area
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
     }
 
+    // Når Bird rammer et objekt med kollision, er det game over.
     private void OnCollisionEnter2D(Collision2D collision)
     {
         FailState();
     }
+
+    // Når Bird forlader trigger objektet på lag seks, som er Game Area, går spildet tabt
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.layer == 6)
@@ -41,7 +50,8 @@ public class BirdScript : MonoBehaviour
 
     private void FailState()
     {
-        logic.GameOver();
         birdIsAlive = false;
+        logic.GameOver();
+
     }
 }
--- Flappy Bird/Assets/LogicScript.cs
+++ Flappy Bird/Assets/LogicScript.cs
@@ -9,30 +9,34 @@ public class LogicScript : MonoBehaviour
     public int playerScore;
     public Text ScoreText;
     public GameObject gameOverScreen;
+    // PointGain bliver false når det er game over, for at undgå at tjene flere point mens man er død
+    public bool PointGain = true;
 
     // ContextMenu tilføjer en knap til at teste metoden
-/// <summary>
-/// tilføjer point til score og opdatere UI til den nye score
-/// </summary>
-/// <param name="scoreToAdd"></param>
+    /// <summary>
+    /// tilføjer point til score og opdatere UI til den nye score
+    /// </summary>
+    /// <param name="scoreToAdd"></param>
     [ContextMenu("Increase Score")]
     public void AddScore(int scoreToAdd)
     {
-        playerScore += scoreToAdd;
-        ScoreText.text = playerScore.ToString();
+        if (PointGain)
+        {
+            playerScore += scoreToAdd;
+            ScoreText.text = playerScore.ToString();
+        }
     }
 
-    /// <summary>
-    /// Genstarter spillet
-    /// </summary>
     public void RestartGame()
     {
+        // Henter den samme scene for at gæninlæse spillet
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     // Viser GameOver UI komponentet
     public void GameOver()
     {
+        PointGain = false;
         gameOverScreen.SetActive(true);
     }
 }
