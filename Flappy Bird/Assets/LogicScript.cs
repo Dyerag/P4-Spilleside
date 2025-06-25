9dc082501ab4a80217e384864a3cba2b5002581d
Dylan <122345584+Dyerag@users.noreply.github.com>
2025-06-25 09:18:26 +0200

    Lavet en ny scene til start menu

--- Flappy Bird/Assets/LogicScript.cs
+++ Flappy Bird/Assets/LogicScript.cs
@@ -1,16 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LogicScript : MonoBehaviour
 {
-    public int playerScore;
-    public Text ScoreText;
-    public GameObject gameOverScreen;
     // PointGain bliver false når det er game over, for at undgå at tjene flere point mens man er død
     public bool PointGain = true;
+    public int playerScore;
+    public Text ScoreText;
+    public GameObject ScoreDisplay;
+    public GameObject gameOverScreen;
+    public GameObject Bird;
+    public GameObject PipeSpawner;
+    public GameObject StartMenu;
 
     // ContextMenu tilføjer en knap til at teste metoden
     /// <summary>
